d14ba89a33ce984719c0256c725b73131bdbff91
anna <annavgk@gmail.com>
2024-10-06 20:36:59 -0600

    Added roof and player to level editor
    Roof automatically calculated to level size, fixed ground to work the same. Added player to the level editor so we can define where player spawns.

--- 2024Capstone/ScrollsLike/Assets/_Main/Scripts/DungeonLevelLoader.cs
+++ 2024Capstone/ScrollsLike/Assets/_Main/Scripts/DungeonLevelLoader.cs
@@ -5,12 +5,16 @@ using Newtonsoft.Json;
 public class DungeonLevelLoader : MonoBehaviour
 {
     public GameObject[] objectPrefabs;
+    public GameObject roofPrefab;
+    public GameObject groundPrefab; 
     private LevelData levelData;
 
     void Start()
     {
         LoadLevel();
+        InstantiateGround();
         InstantiateObjects();
+        InstantiateRoof();
     }
 
     void LoadLevel()
@@ -27,6 +31,18 @@ public class DungeonLevelLoader : MonoBehaviour
         }
     }
 
+    void InstantiateGround()
+    {
+        for (int i = 0; i < levelData.levelWidth; i++)
+        {
+            for (int j = 0; j < levelData.levelHeight; j++)
+            {
+                Vector3 groundPosition = new Vector3(i, 0, j);
+                Instantiate(groundPrefab, groundPosition, Quaternion.identity);
+            }
+        }
+    }
+
     void InstantiateObjects()
     {
         for (int i = 0; i < levelData.levelWidth; i++)
@@ -36,10 +52,23 @@ public class DungeonLevelLoader : MonoBehaviour
                 int objectType = levelData.grid[i, j]; // The grid value
                 if (objectType != -1) // -1 is an empty space
                 {
-                    Vector3 spawnPosition = new Vector3(i, 0, j); // Adjust per game grid size
+                    Vector3 spawnPosition = new Vector3(i, 0, j); 
                     // Instantiate the object 
                     Instantiate(objectPrefabs[objectType], spawnPosition, Quaternion.identity);
                 }
+            }
+        }
+    }
+
+    void InstantiateRoof()
+    {
+        int roofHeight = 3;
+        for (int i = 0; i < levelData.levelWidth; i++)
+        {
+            for (int j = 0; j < levelData.levelHeight; j++)
+            {
+                Vector3 roofPosition = new Vector3(i, roofHeight, j);
+                Instantiate(roofPrefab, roofPosition, Quaternion.identity);
             }
         }
     }
