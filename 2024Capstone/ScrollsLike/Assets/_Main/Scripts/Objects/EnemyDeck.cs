df02fe8455668f1599afb8f2716cc10d0b508786
Blake Laprade <lapradeblake@gmail.com>
2024-10-11 22:00:53 -0600

    enemy card and deck data

---
+++ 2024Capstone/ScrollsLike/Assets/_Main/Scripts/Objects/EnemyDeck.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyDeckData", menuName = "Enemies/EnemyDeckData")]
+public class EnemyDeck : ScriptableObject
+{
+    [SerializeField] private List<CardData> _startingCards; //the cards the player will start with at the beginning of the game
+    public List<EnemyCardData> Deck { get { return _deck; } }
+    private List<EnemyCardData> _deck = new List<EnemyCardData>();
+}
