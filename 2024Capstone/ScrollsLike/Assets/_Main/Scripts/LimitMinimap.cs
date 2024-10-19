ea310587fba664f66fba23b32a653a2db356e143
anna <annavgk@gmail.com>
2024-10-18 20:48:06 -0600

    Implemented minimap
    Textures can be changed (placeholders)

---
+++ 2024Capstone/ScrollsLike/Assets/_Main/Scripts/LimitMinimap.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitMinimap : MonoBehaviour
+{
+    public GameObject player;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, 10, player.transform.position.z);
+        transform.rotation = Quaternion.Euler(90, 0, 0);
+    }
+}
