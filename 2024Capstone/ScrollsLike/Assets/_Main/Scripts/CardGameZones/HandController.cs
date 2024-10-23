5efada49c7fd4d87685dbd77456da41abeca555f
Blake Laprade <lapradeblake@gmail.com>
2024-10-23 11:46:43 -0600

    texturing fixes

--- 2024Capstone/ScrollsLike/Assets/_Main/Scripts/CardGameZones/HandController.cs
+++ 2024Capstone/ScrollsLike/Assets/_Main/Scripts/CardGameZones/HandController.cs
@@ -68,6 +68,7 @@ public class HandController : Singleton<HandController>
 
             if (_cardsInHand.Count < _maxCardsInHand)
             {
+                bool trigger = false;
                 CardGameManager.Instance.DrawCard();
                 yield return new WaitForSeconds(_drawDelay);
             }
