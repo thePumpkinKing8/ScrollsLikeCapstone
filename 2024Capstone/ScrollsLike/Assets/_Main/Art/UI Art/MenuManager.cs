6cb057bf68eab020aa279ce5d0770831166e574e
Cansugc <cansuguccg@gmail.com>
2024-10-28 02:46:38 -0400

    Added Splash Screen

--- 2024Capstone/ScrollsLike/Assets/_Main/Art/UI Art/MenuManager.cs
+++ 2024Capstone/ScrollsLike/Assets/_Main/Art/UI Art/MenuManager.cs
@@ -1,25 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
+
+    [SerializeField] private GameObject splashScreen;
+    [SerializeField] private GameObject mainMenu;
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            splashScreen.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
     public void Play()
     {
         SceneManager.LoadScene(1);
     }
-    public void Exit()
-    {
-        Application.Quit();
-    }
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            SceneManager.LoadScene(2);
 
-        }
+    void OnClick()
+    {
+
     }
-    
+
+
+    public void Quit()
+    {
+
+#if UNITY_EDITOR
+        if (EditorApplication.isPlaying)
+        {
+            EditorApplication.isPlaying = false;
+        }
+
+#else
+        Application.Quit();
+#endif
+
+
+    }
 }
