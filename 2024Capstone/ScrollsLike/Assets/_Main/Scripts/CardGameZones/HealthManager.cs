5015af2872573e0ccacd5daa3131c56b37e35c01
woaoh <woaohwee@gmail.com>
2024-12-02 15:43:31 -0700

    the making of the tutorial

--- 2024Capstone/ScrollsLike/Assets/_Main/Scripts/CardGameZones/HealthManager.cs
+++ 2024Capstone/ScrollsLike/Assets/_Main/Scripts/CardGameZones/HealthManager.cs
@@ -80,7 +80,7 @@ public class HealthManager : Singleton<HealthManager>, ICardEffectable
             _fillableBar.fillAmount = ((float)PlayerHealth / (float)_startingHealth);
             _statText.text = PlayerHealth.ToString();
         }
-        _poisonText.text = $"Poison:{Poison}";
+        _poisonText.text = $"{Poison}";
 
         if(PlayerHealth > _startingHealth)
         {
