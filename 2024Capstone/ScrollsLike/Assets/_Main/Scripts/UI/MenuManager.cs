8c408933fc2f6488b32ea191dfeba8eaebd17b81
Blake Laprade <lapradeblake@gmail.com>
2024-12-02 19:53:30 -0700

    bunch of stuff

--- 2024Capstone/ScrollsLike/Assets/_Main/Scripts/UI/MenuManager.cs
+++ 2024Capstone/ScrollsLike/Assets/_Main/Scripts/UI/MenuManager.cs
@@ -35,16 +35,7 @@ public class MenuManager : MonoBehaviour
     public void Quit()
     {
 
-#if UNITY_EDITOR
-        if (EditorApplication.isPlaying)
-        {
-            EditorApplication.isPlaying = false;
-        }
-
-#else
         Application.Quit();
-#endif
-
 
     }
 }
