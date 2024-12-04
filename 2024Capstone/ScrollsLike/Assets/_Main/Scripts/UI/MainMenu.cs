5e68824cf98f62701f4a57e2f45ade95246a8119
woaoh <woaohwee@gmail.com>
2024-12-03 20:19:53 -0700

    main menu and tutorial working
    scuff loading into game

---
+++ 2024Capstone/ScrollsLike/Assets/_Main/Scripts/UI/MainMenu.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("UI Pages")]
+    [SerializeField]
+    private GameObject mainMenu;
+    [SerializeField]
+    private GameObject tutorial;
+    [SerializeField]
+    private GameObject options;
+
+    [Header("Main Menu Buttons")]
+    [SerializeField]
+    private Button startButton;
+    [SerializeField]
+    private Button tutorialButton;
+    [SerializeField]
+    private Button optionsButton;
+    [SerializeField]
+    private Button exitButton;
+
+    [SerializeField]
+    private List<Button> returnButtons;
+
+    void Start()
+    {
+        EnableMainMenu();
+
+        startButton.onClick.AddListener(StartGame);
+        optionsButton.onClick.AddListener(EnableOptions);
+        tutorialButton.onClick.AddListener(Tutorial);
+        exitButton.onClick.AddListener(OnExitGame);
+
+        foreach (var item in returnButtons)
+        {
+            item.onClick.AddListener(EnableMainMenu);
+        }
+    }
+
+    public void StartGame()
+    {
+        HideAll();
+        SceneManager.LoadScene("Anna_Gym");
+    }
+
+    public void HideAll()
+    {
+        mainMenu.SetActive(false);
+        tutorial.SetActive(false);
+        options.SetActive(false);
+    }
+
+    public void EnableMainMenu()
+    {
+        mainMenu.SetActive(true);
+        tutorial.SetActive(false);
+        options.SetActive(false);
+    }
+
+    public void EnableOptions()
+    {
+        mainMenu.SetActive(true);
+        tutorial.SetActive(false);
+        options.SetActive(true);
+    }
+    public void Tutorial()
+    {
+        mainMenu.SetActive(false);
+        tutorial.SetActive(true);
+        options.SetActive(false);
+    }
+
+    public void OnExitGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+}
